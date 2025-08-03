78406cf81fcf459bf6410c3f27c8445f902f16cb
mahopon <tan.cy@outlook.com>
2025-08-03 22:00:07 +0000

    Auto-update 03-08-25

--- backup-repos.sh
+++ backup-repos.sh
@@ -18,10 +18,10 @@ fi
 while IFS= read -r path;
 do
     cd "$path" || { echo "Unable to cd to $path"; continue; }
+    git pull > /dev/null 2>&1
     if git status --porcelain | grep -q .; then
-        git pull > /dev/null 2>&1
         git add .
-        git commit -m "Auto-update $(date +"%d-%m-%y %H:%M")" > /dev/null 2>&1
+        git commit -m "Auto-update $(date +"%d-%m-%y")" > /dev/null 2>&1
         git push > /dev/null 2>&1
         echo "Auto pushed updates for $path"
     else
